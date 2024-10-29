066a46a3e32281f455ea4769a11b4444ea6f31f7
jenellebankas <139067450+jenellebankas@users.noreply.github.com>
2024-10-29 12:48:30 +0000

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jenelle Bankas on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
